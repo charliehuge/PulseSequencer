1a01b331f373c99b95d99f6da619966bdda133b6
Charlie Huguenard <charlie.huguenard@gmail.com>
2015-10-20 13:14:45 -0700

    added step sequencer example

---
+++ Assets/PulseSequencer/Examples/Scripts/CycleMaterialColor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CycleMaterialColor : MonoBehaviour
+{
+    [SerializeField] private Color[] _colors;
+
+    [SerializeField] private Renderer _renderer;
+
+    private int _currentColorIdx;
+
+    private void OnEnable()
+    {
+        _currentColorIdx = 0;
+        CycleColor();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        CycleColor();
+    }
+
+    private void CycleColor()
+    {
+        if (_colors == null || _colors.Length == 0 || _renderer == null)
+        {
+            return;
+        }
+
+        _renderer.material.color = _colors[_currentColorIdx];
+        _currentColorIdx = (_currentColorIdx + 1) % _colors.Length;
+    }
+}
---
+++ Assets/PulseSequencer/Examples/Scripts/ToggleSuspend.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using DerelictComputer;
+
+public class ToggleSuspend : MonoBehaviour
+{
+    [SerializeField] private PatternFollowerGroup _patternFollowerGroup;
+
+    private void OnMouseUpAsButton()
+    {
+        _patternFollowerGroup.Suspended = !_patternFollowerGroup.Suspended;
+    }
+}
